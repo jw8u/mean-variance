94323dd4433edc478ce09685c4b2aa286c9f366c
jw8u <76448798+jw8u@users.noreply.github.com>
2022-12-14 14:00:32 -0700

    Update primary.R

--- primary.R
+++ primary.R
@@ -4,7 +4,7 @@
 #B = population size
 #num_rep = number of sample repetitions
 
-#Usage
+#functions:
 #mv_stock(n,k,risk_coef)
 #simulation_function(k,risk_coef,n,B,num_rep)
 
@@ -17,5 +17,14 @@ mv_stock(n=78,k=10,risk_coef=50)
 mv_stock(n=104,k=10,risk_coef=50)
 mv_stock(n=130,k=10,risk_coef=50)
 
-#changing risk aversion coefficient
 #changing k
+mv_stock(n=130,k=5,risk_coef=50)
+mv_stock(n=130,k=10,risk_coef=50)
+mv_stock(n=130,k=25,risk_coef=50)
+mv_stock(n=130,k=40,risk_coef=50)
+
+#changing risk aversion coefficient
+mv_stock(n=130,k=10,risk_coef=10)
+mv_stock(n=130,k=10,risk_coef=25)
+mv_stock(n=130,k=10,risk_coef=50)
+mv_stock(n=130,k=10,risk_coef=100)
