8f838e76439dc8b9d4186ed66f20554d306576ce
jw8u <76448798+jw8u@users.noreply.github.com>
2022-12-14 14:13:42 -0700

    Update functions.R

--- functions.R
+++ functions.R
@@ -2,7 +2,6 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(MASS, quantmod, BatchGetSymbols)
 
 mv_stock = function(n,k,risk_coef){
-  #vector of ones
   one_vector = rep(1,k)
   
   #get stock data
